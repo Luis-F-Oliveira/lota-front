fa303f8303106a9b27a4242a7d47ceb33de4d6be
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-02-14 16:47:31 -0400

    initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><h1>Home</h1></div>,
+    errorElement: <div>Error Page</div>
+  }
+])
